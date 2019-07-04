b09a5c780892f7bffbccb9d395aaf7d8731047de
Paulius Sabeckis <paulius.sabeckis@devbridge.com>
2019-07-04 15:35:50 +0300

    Update dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   coverageDirectory: '.coverage',
   collectCoverageFrom: ['src/**/*.{js}', '!src/**/index.js'],
   setupFiles: [],
-  testMatch: ['<rootDir>/src/**/__tests__/**/*.spec.{js}', '<rootDir>/src/**/?(*.)(spec).{js}'],
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.spec.js', '<rootDir>/src/**/?(*.)(spec|test).js'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
